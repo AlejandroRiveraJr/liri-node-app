4cad88e0a8e6a1433e4e9b255505eba12edba3c1
Alejandro Rivera Jr <a.rivera7122@gmail.com>
2018-04-20 01:54:33 -0400

    npm

--- liriNodeApp.js
+++ liriNodeApp.js
@@ -1,2 +1,5 @@
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var keys = require("./keys.js");
+var request = require("request");
+var spotify = require("spotify")
+var Twitter = require('twitter');
+var fs = require("fs");
