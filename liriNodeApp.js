07cf8982c48773394e94f2e51cba08c6e5d67057
Alejandro Rivera Jr <a.rivera7122@gmail.com>
2018-04-20 18:54:57 -0400

    added twitter

--- liriNodeApp.js
+++ liriNodeApp.js
@@ -1,6 +1,27 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var request = require("request");
-var spotify = require("spotify")
+// key module
+var keys = require('./keys.js');
+//require npm packages
 var Twitter = require('twitter');
-var fs = require("fs");
+var Spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+
+var client = new twitter(keys.twitter);
+
+console.log(keys.twitter);
+
+function twitter() {
+    var params = { screen_name: 'rabbithole', count: 20 };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at + "\n" + tweets[i].text);
+                console.log("_____________________________");
+            }
+        } else {
+            console.log(error);
+        }
+    });
+};
